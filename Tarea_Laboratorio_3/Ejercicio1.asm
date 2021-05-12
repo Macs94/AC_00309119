e35c5b193577fb983bd790712bccf42c321ba238
Macs94 <00309119@uca.edu.sv>
2021-05-11 21:42:14 -0600

    Add Ejercicio1_Labo3

---
+++ Tarea_Laboratorio_3/Ejercicio1.asm
@@ -0,0 +1,30 @@
+org 100h
+
+section .text
+
+mov     byte [201h], 03d
+mov     byte [202h], 09d
+mov     byte [203h], 01d
+mov     byte [204h], 01d
+mov     byte [205h], 09d
+
+mov CX,08d
+mov AX,0000d
+mov BX,0000d
+mov SI,200h
+
+iterar:
+        mov BL,[SI]
+        add AX,BX
+        inc SI
+        dec CX
+        jnz iterar
+
+mov DI,[210h]
+mov [DI],AX
+mov BX,08d
+div BX
+mov [20Ah],AX
+
+
+int 20h
